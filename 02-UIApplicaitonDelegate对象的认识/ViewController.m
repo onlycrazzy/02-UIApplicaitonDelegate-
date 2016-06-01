b4602a1876f7441834a8ca7e7db4d9c3bf44612f
WWHB <ioswth@gmail.com>
2016-06-01 13:19:19 +0800

    正常的提交
    实现功能目标

--- 02-UIApplicaitonDelegate对象的认识/ViewController.m
+++ 02-UIApplicaitonDelegate对象的认识/ViewController.m
@@ -19,6 +19,11 @@
     // Do any additional setup after loading the view, typically from a nib.
 }
 
+- (void)touchesBegan:(NSSet<UITouch *> *)touches withEvent:(UIEvent *)event {
+    NSLog(@"zuolegaibian");
+}
+
+
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
